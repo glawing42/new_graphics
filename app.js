8a146db69423abec7837a21266b6273543aae83c
Graham Lawing <graham.lawing@gmail.com>
2017-11-05 01:27:33 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,162 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var nodemailer = require('nodemailer');
+
+
+// mongoose.connect('mongodb://localhost/graphicsapp');
+
+// var db = mongoose.connection;
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/graphicsapp";
+
+// Connect mongoose to our datbase
+mongoose.connect(db, function(error) {
+    // Log any errors connecting with mongoose
+    if (error) {
+        console.log(error);
+    }
+    // Or log a success message
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
+
+//   Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+  });
+  app.get('/', (req, res) => {
+    res.render('Home');
+});
+
+  app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/send',(req, res) => {
+  
+  const output = `
+  <p>You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Company: ${req.body.company}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Phone Number: ${req.body.phone}</li>
+  </ul>
+  <h3>Message</h3>
+  <p>${req.body.message}</p>
+  `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+          user: 'yf55l5zfan2yguma@ethereal.email',
+          pass: 'ktVpMPNdChNCkCgjgw'
+      },
+      tls:{
+        rejectUnauthorized:false
+      }      
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nodemailer Contact 👻" <graham.lawing@gmail.com>', // sender address
+      to: 'graham.lawing@gmail.com', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {msg: 'Email has been sent'});
+  });
+});
+
+
+  app.use('/', routes);
+  app.use('/users', users);
+
+
+  
+  // Set Port
+  app.set('port', (process.env.PORT || 3000));
+  
+  app.listen(app.get('port'), function(){
+      console.log('Server started on port '+app.get('port'));
+  });
